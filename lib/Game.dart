1d2f4183129bc53255b70b0ea9f07443dbed15e8
Andre Ricardo <andrericardoweb@gmail.com>
2020-06-13 15:20:42 -0300

    Random ChooseApp

--- lib/Game.dart
+++ lib/Game.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Game extends StatefulWidget {
   @override
@@ -6,19 +7,40 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
-
   var _imageApp = AssetImage("images/padrao.png");
 
-  void _optionSelected (String chooseUser) {
-    print("Opção selecionada: " + chooseUser);
+  void _optionSelected(String chooseUser) {
+    var options = ["pedra", "papel", "tesoura"];
+    var optionNumber = Random().nextInt(3);
+    var chooseApp = options[optionNumber];
+
+    print("Escolha do App:" + chooseApp);
+    print("Escolha do usuario:" + chooseUser);
+
+    //Displays image chosen by the app
+    switch (chooseApp) {
+      case "pedra":
+        setState(() {
+          this._imageApp = AssetImage("images/pedra.png");
+        });
+        break;
+      case "papel":
+        setState(() {
+          this._imageApp = AssetImage("images/papel.png");
+        });
+        break;
+      case "tesoura":
+        setState(() {
+          this._imageApp = AssetImage("images/tesoura.png");
+        });
+        break;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("JokenPo")
-      ),
+      appBar: AppBar(title: Text("JokenPo")),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
@@ -28,10 +50,7 @@ class _GameState extends State<Game> {
             child: Text(
               "Escolha do App",
               textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold
-              ),
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
           //image
@@ -42,10 +61,7 @@ class _GameState extends State<Game> {
             child: Text(
               "Escolha uma opção abaixo",
               textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold
-              ),
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
           Row(
@@ -53,15 +69,24 @@ class _GameState extends State<Game> {
             children: <Widget>[
               GestureDetector(
                 onTap: () => _optionSelected("pedra"),
-                child: Image.asset("images/pedra.png", height: 100,),
+                child: Image.asset(
+                  "images/pedra.png",
+                  height: 100,
+                ),
               ),
               GestureDetector(
                 onTap: () => _optionSelected("papel"),
-                child: Image.asset("images/papel.png", height: 100,),
+                child: Image.asset(
+                  "images/papel.png",
+                  height: 100,
+                ),
               ),
               GestureDetector(
                 onTap: () => _optionSelected("tesoura"),
-                child: Image.asset("images/tesoura.png", height: 100,),
+                child: Image.asset(
+                  "images/tesoura.png",
+                  height: 100,
+                ),
               )
             ],
           )
